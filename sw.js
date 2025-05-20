0d8f6a85dd23856dbf454af543d74c2da2267d35
kha-HH <kirsten@nachtzuschlag.de>
2025-05-20 16:47:25 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,6 +7,7 @@ const ASSETS_TO_CACHE = [
   // ggf. weitere Assets: CSS, JS etc.
 ];
 
+// Install-Event: Initiale Dateien cachen
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
@@ -14,17 +15,20 @@ self.addEventListener('install', event => {
   self.skipWaiting();
 });
 
+// Activate-Event: Alte Caches aufräumen
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
       )
     )
   );
   self.clients.claim();
 });
 
+// Fetch-Event: Erst aus dem Cache, dann aus dem Netz
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request, {ignoreSearch: true}).then(response => {
